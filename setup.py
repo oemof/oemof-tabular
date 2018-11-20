da4cee837be7bdc725145aafedde11cb58d6625d
Stephan Günther <gnn.code@gmail.com>
2018-11-20 16:53:46 +0100

    Wrap lines to 79 characters

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(
     license='BSD 3-Clause License',
     description='Load oemof energy systems from tabular data sources.',
     long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S)
+          .sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
     ),
     author='Stephan Günther',
@@ -41,7 +42,10 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # complete classifier list:
+        #
+        #   http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
