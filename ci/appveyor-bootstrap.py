1bd49cf4559401e1b8a40b762a9f8f79f32c5d49
Stephan Günther <gnn.code@gmail.com>
2018-11-22 01:09:56 +0100

    Remove Python 2.7 stuff
    I don't really want to support that version, so there's no need to have
    it in the continuous integration builds or anywhere else. Hope I did
    catch every occurrence.

--- ci/appveyor-bootstrap.py
+++ ci/appveyor-bootstrap.py
@@ -18,8 +18,6 @@ BASE_URL = "https://www.python.org/ftp/python/"
 GET_PIP_URL = "https://bootstrap.pypa.io/get-pip.py"
 GET_PIP_PATH = "C:\get-pip.py"
 URLS = {
-    ("2.7", "64"): BASE_URL + "2.7.13/python-2.7.13.amd64.msi",
-    ("2.7", "32"): BASE_URL + "2.7.13/python-2.7.13.msi",
     ("3.4", "64"): BASE_URL + "3.4.4/python-3.4.4.amd64.msi",
     ("3.4", "32"): BASE_URL + "3.4.4/python-3.4.4.msi",
     ("3.5", "64"): BASE_URL + "3.5.4/python-3.5.4-amd64.exe",
@@ -29,8 +27,6 @@ URLS = {
 }
 INSTALL_CMD = {
     # Commands are allowed to fail only if they are not the last command.  Eg: uninstall (/x) allowed to fail.
-    "2.7": [["msiexec.exe", "/L*+!", "install.log", "/qn", "/x", "{path}"],
-            ["msiexec.exe", "/L*+!", "install.log", "/qn", "/i", "{path}", "TARGETDIR={home}"]],
     "3.4": [["msiexec.exe", "/L*+!", "install.log", "/qn", "/x", "{path}"],
             ["msiexec.exe", "/L*+!", "install.log", "/qn", "/i", "{path}", "TARGETDIR={home}"]],
     "3.5": [["{path}", "/quiet", "TargetDir={home}"]],
