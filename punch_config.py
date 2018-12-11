9da70d164347cd676fc15871323aed21735582db
Stephan Günther <gnn.code@gmail.com>
2018-12-11 13:41:32 +0100

    Use `punch.py` instead of `bumpversion`
    Let's hope this one works as expected.

---
+++ punch_config.py
@@ -0,0 +1,14 @@
+__config_version__ = 1
+
+GLOBALS = {
+    'serializer': '{{major}}.{{minor}}.{{patch}}{{status if status}}',
+}
+
+FILES = ['README.rst', 'docs/conf.py', 'setup.py',
+         'src/oemof/tabular/__init__.py']
+
+VERSION = ['major', 'minor', 'patch',
+           {'name': 'status',
+            'type': 'value_list',
+            'allowed_values': ['', 'dev']}]
+
