8af4472e592e292db29ad552fe48d98685d5524f
simnh <simon.hilpert@uni-flensburg.de>
2018-12-12 15:24:57 +0100

    Rewrite text for easier debugging

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -9,16 +9,19 @@ from oemof.tabular.facades import TYPEMAP
 def test_example_datapackage_readability():
     """ The example datapackages can be read and loaded.
     """
-    systems = [
-        deserialize_energy_system(
-            ES,
-            pkg.resource_filename(
-                'oemof.tabular',
-                'examples/datapackages/{}/datapackage.json'.format(example)),
-            typemap=TYPEMAP)
-        for example in pkg.resource_listdir(
-            'oemof.tabular', 'examples/datapackages'
-        )]
+
+    systems = []
+    for example in pkg.resource_listdir(
+        'oemof.tabular', 'examples/datapackages'):
+
+        systems.append(
+            deserialize_energy_system(
+                ES,
+                pkg.resource_filename(
+                    'oemof.tabular',
+                    'examples/datapackages/{}/datapackage.json'.format(example)),
+                typemap=TYPEMAP)
+            )
+
     for system in systems:
         assert (type(system) is ES)
-
