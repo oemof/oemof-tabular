27abee19ead957a071e366d120b2a5442a44e9df
Stephan Günther <gnn.code@gmail.com>
2018-12-13 18:35:28 +0100

    Blacken setup.py

--- setup.py
+++ setup.py
@@ -13,27 +13,29 @@ from setuptools import find_packages, setup
 def read(*names, **kwargs):
     with io.open(
         join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        encoding=kwargs.get("encoding", "utf8"),
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='oemof.tabular',
-    version='0.0.1dev',
-    license='BSD 3-Clause License',
-    description='Load oemof energy systems from tabular data sources.',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S)
-          .sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="oemof.tabular",
+    version="0.0.1dev",
+    license="BSD 3-Clause License",
+    description="Load oemof energy systems from tabular data sources.",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='Stephan Günther, Simon Hilpert, Martin Söthe, Cord Kaldemeyer',
-    author_email='gnn.code@gmail.com',
-    url='https://github.com/oemof/oemof-tabular',
-    packages=['oemof'] + ['oemof.' + p for p in find_packages('src/oemof')],
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Stephan Günther, Simon Hilpert, Martin Söthe, Cord Kaldemeyer",
+    author_email="gnn.code@gmail.com",
+    url="https://github.com/oemof/oemof-tabular",
+    packages=["oemof"] + ["oemof." + p for p in find_packages("src/oemof")],
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
@@ -41,39 +43,39 @@ setup(
         #
         #   http://pypi.python.org/pypi?%3Aaction=list_classifiers
         #
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',
         # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        "Topic :: Utilities",
     ],
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        'datapackage',
-        'geojson',
-        'oemof==0.2.4.dev0',
-        'pandas>=0.22',
-        'paramiko',
-        'pyproj',
-        'pyshp',
-        'scipy',
-        'shapely',
-        'tsam',
+        "datapackage",
+        "geojson",
+        "oemof==0.2.4.dev0",
+        "pandas>=0.22",
+        "paramiko",
+        "pyproj",
+        "pyshp",
+        "scipy",
+        "shapely",
+        "tsam",
     ],
     extras_require={
         # eg:
@@ -81,7 +83,10 @@ setup(
         #   ':python_version=="2.6"': ['argparse'],
     },
     dependency_links=[
-        ('git+https://git@github.com/oemof/oemof.git'
-         '@features/move-datapackage-reader-into-standalone-package'
-         '#egg=oemof-0.2.4.dev0')],
+        (
+            "git+https://git@github.com/oemof/oemof.git"
+            "@features/move-datapackage-reader-into-standalone-package"
+            "#egg=oemof-0.2.4.dev0"
+        )
+    ],
 )
