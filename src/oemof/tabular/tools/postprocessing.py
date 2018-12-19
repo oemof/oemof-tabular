f4a0ad880917ad1a51e102bf1ff79754d0d10e53
simnh <simon.hilpert@uni-flensburg.de>
2018-12-19 08:16:30 +0100

    Pepify postprocessing with black

--- src/oemof/tabular/tools/postprocessing.py
+++ src/oemof/tabular/tools/postprocessing.py
@@ -11,7 +11,7 @@ from oemof.outputlib import views, processing
 from oemof.tabular import facades
 
 
-def component_results(es, results, select='sequences'):
+def component_results(es, results, select="sequences"):
     """ Aggregated by component type
     """
 
@@ -19,28 +19,37 @@ def component_results(es, results, select='sequences'):
 
     for k, v in es.typemap.items():
         if type(k) == str:
-            if select == 'sequences':
+            if select == "sequences":
                 _seq_by_type = [
-                    views.node(results, n, multiindex=True).get('sequences')
-                    for n in es.nodes if isinstance(n, v) and not isinstance(n, Bus)]
+                    views.node(results, n, multiindex=True).get("sequences")
+                    for n in es.nodes
+                    if isinstance(n, v) and not isinstance(n, Bus)
+                ]
                 # check if dataframes / series have been returned
-                if any([isinstance(i, (pd.DataFrame, pd.Series))
-                       for i in _seq_by_type]):
-                    seq_by_type =  pd.concat(_seq_by_type, axis=1)
+                if any(
+                    [
+                        isinstance(i, (pd.DataFrame, pd.Series))
+                        for i in _seq_by_type
+                    ]
+                ):
+                    seq_by_type = pd.concat(_seq_by_type, axis=1)
                     c[str(k)] = seq_by_type
 
-            if select == 'scalars':
+            if select == "scalars":
                 _sca_by_type = [
-                    views.node(results, n, multiindex=True).get('scalars')
-                    for n in es.nodes if isinstance(n, v) and not isinstance(n, Bus)]
+                    views.node(results, n, multiindex=True).get("scalars")
+                    for n in es.nodes
+                    if isinstance(n, v) and not isinstance(n, Bus)
+                ]
 
                 if [x for x in _sca_by_type if x is not None]:
-                    sca_by_type =  pd.concat(_sca_by_type)
+                    sca_by_type = pd.concat(_sca_by_type)
                     c[str(k)] = _sca_by_type
 
     return c
 
-def bus_results(es, results, select='sequences', concat=False):
+
+def bus_results(es, results, select="sequences", concat=False):
     """ Aggregated for every bus of the energy system
     """
     br = {}
@@ -48,16 +57,21 @@ def bus_results(es, results, select='sequences', concat=False):
     buses = [b for b in es.nodes if isinstance(b, Bus)]
 
     for b in buses:
-        if select == 'sequences':
-            bus_sequences = pd.concat([
-                views.node(results, b, multiindex=True).get('sequences',
-                                                            pd.DataFrame())], axis=1)
+        if select == "sequences":
+            bus_sequences = pd.concat(
+                [
+                    views.node(results, b, multiindex=True).get(
+                        "sequences", pd.DataFrame()
+                    )
+                ],
+                axis=1,
+            )
             br[str(b)] = bus_sequences
-        if select == 'scalars':
-            br[str(b)] = views.node(results, b, multiindex=True).get('scalars', pd.DataFrame())
+        if select == "scalars":
+            br[str(b)] = views.node(results, b, multiindex=True).get("scalars")
 
     if concat:
-        if select == 'sequences':
+        if select == "sequences":
             axis = 1
         else:
             axis = 0
@@ -65,44 +79,58 @@ def bus_results(es, results, select='sequences', concat=False):
 
     return br
 
-def supply_results(types=['dispatchable', 'volatile', 'conversion',
-                          'backpressure', 'extraction', 'storage',
-                          'generator', 'reservoir'],
-                   bus=None, results=None, es=None):
+
+def supply_results(
+    types=[
+        "dispatchable",
+        "volatile",
+        "conversion",
+        "backpressure",
+        "extraction",
+        "storage",
+        "generator",
+        "reservoir",
+    ],
+    bus=None,
+    results=None,
+    es=None,
+):
     """
     """
     selection = pd.DataFrame()
 
     for t in types:
         if isinstance(es.typemap[t], GenericStorage):
-            df = views.net_storage_flow(results,
-                                        node_type=es.typemap[t])
+            df = views.net_storage_flow(results, node_type=es.typemap[t])
             if df is not None:
                 selection = pd.concat([selection, df], axis=1)
         else:
-            df = views.node_output_by_type(results,
-                                           node_type=es.typemap[t])
+            df = views.node_output_by_type(results, node_type=es.typemap[t])
             if df is not None:
                 selection = pd.concat([selection, df], axis=1)
 
-    selection = selection.loc[:, (slice(None),
-                                  [es.groups[b] for b in bus],
-                                  ['flow', 'net_flow'])]
+    selection = selection.loc[
+        :, (slice(None), [es.groups[b] for b in bus], ["flow", "net_flow"])
+    ]
     return selection
 
-def demand_results(types=['load'], bus=None, results=None, es=None):
+
+def demand_results(types=["load"], bus=None, results=None, es=None):
     """
     """
     selection = pd.DataFrame()
 
     for t in types:
         selection = pd.concat(
-            [selection,
-             views.node_input_by_type(results, node_type=es.typemap[t])],
-             axis=1)
+            [
+                selection,
+                views.node_input_by_type(results, node_type=es.typemap[t]),
+            ],
+            axis=1,
+        )
 
-    selection = selection.loc[:, ([es.groups[b] for b in bus],
-                                 slice(None),
-                                 ['flow'])]
+    selection = selection.loc[
+        :, ([es.groups[b] for b in bus], slice(None), ["flow"])
+    ]
 
     return selection
