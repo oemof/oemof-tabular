4aafe5290b8b8f8a2d016c214240780ce5e186fa
simnh <simon.hilpert@uni-flensburg.de>
2018-12-20 11:22:07 +0100

    Add command line tool for oemof-tabular

--- setup.py
+++ setup.py
@@ -79,11 +79,17 @@ setup(
         "scipy",
         "shapely",
         "tsam",
+        "click"
     ],
     extras_require={
         # eg:
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
+    },
+    entry_points={
+        'console_scripts': [
+            'ota = oemof.tabular.cli:main',
+        ]
     },
     dependency_links=(
         [
