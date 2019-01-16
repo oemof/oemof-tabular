e1a93c4898a0ecfe211fba1d3352bd3b488fad1c
Stephan Günther <gnn.code@gmail.com>
2019-01-17 00:53:48 +0100

    Stop listing Python 3.7 as supported

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
