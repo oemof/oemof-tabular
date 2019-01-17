273392434c1d2291426f0db1220804514b4ff7bc
Stephan Günther <gnn.code@gmail.com>
2019-01-17 01:32:35 +0100

    Remove `pyproj` dependency link
    This is only necessary to fix the build for Python 3.7 and the
    corresponding code is preserved on the branch dealing with this exact
    issue.

--- setup.py
+++ setup.py
@@ -72,9 +72,7 @@ setup(
         "oemof==0.2.4.dev0",
         "pandas>=0.22",
         "paramiko",
-        "pyproj==1.9.5.1.dev0"
-        if sys.version_info.major >= 3 and sys.version_info.minor >= 7
-        else "pyproj",
+        "pyproj",
         "pyshp",
         "scipy",
         "shapely",
@@ -85,18 +83,7 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
-    dependency_links=(
-        [
-            (
-                "git+https://git@github.com/gnn/pyproj.git"
-                "@69a26ce46634749f602518a375849999cb5e41e0"
-                "#egg=pyproj-1.9.5.1.dev0"
-            )
-        ]
-        if sys.version_info.major >= 3 and sys.version_info.minor >= 7
-        else []
-    )
-    + [
+    dependency_links=[
         (
             "git+https://git@github.com/oemof/oemof.git"
             "@releases/v0_3_0"
