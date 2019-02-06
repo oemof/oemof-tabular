15801fa2918e605e04b055f73b63e77bde65e4fa
Martin <martin.soethe@uni-flensburg.de>
2019-02-06 18:52:43 +0100

    Fix script

--- src/oemof/tabular/examples/scripting/compute.py
+++ src/oemof/tabular/examples/scripting/compute.py
@@ -3,6 +3,7 @@
 import os
 import pkg_resources as pkg
 
+import oemof.tabular.datapackage
 from oemof.solph import EnergySystem, Model
 import oemof.tabular.tools.postprocessing as pp
 from oemof.tabular.facades import TYPEMAP
