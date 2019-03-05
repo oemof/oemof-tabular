b48c0d5313b18b99df2445db1584cb28c702d40d
Stephan Günther <gnn.code@gmail.com>
2019-03-05 03:39:30 +0100

    Remove unused imports
    The style checker complained about this and instead of making those
    sub-modules attributes of this package, one can just import them
    directly wherever they should be used.

--- src/oemof/tabular/datapackage/__init__.py
+++ src/oemof/tabular/datapackage/__init__.py
@@ -2,6 +2,4 @@ from oemof.energy_system import EnergySystem
 
 from .reading import deserialize_energy_system
 
-from oemof.tabular.datapackage import building, processing, aggregation
-
 EnergySystem.from_datapackage = classmethod(deserialize_energy_system)
