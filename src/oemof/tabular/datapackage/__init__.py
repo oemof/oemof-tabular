3d3a3bc11eecfd6021ddc06c64ece44c661cbaf2
simnh <simon.hilpert@uni-flensburg.de>
2021-05-12 09:39:35 +0200

    Fix error when trying to import datapackage modules

--- src/oemof/tabular/datapackage/__init__.py
+++ src/oemof/tabular/datapackage/__init__.py
@@ -1,5 +1,6 @@
 from oemof.network.energy_system import EnergySystem
 
 from .reading import deserialize_energy_system
+from . import processing, building, aggregation
 
 EnergySystem.from_datapackage = classmethod(deserialize_energy_system)
