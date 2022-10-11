85f297f7edd23641dcea4a501dc444ea71baf9b2
jnnr <32454596+jnnr@users.noreply.github.com>
2022-10-11 15:40:27 +0200

    Re-introduce import to fix test_custom_foreign_keys

--- src/oemof/tabular/datapackage/__init__.py
+++ src/oemof/tabular/datapackage/__init__.py
@@ -1,5 +1,6 @@
 from oemof.network.energy_system import EnergySystem
 
+from . import building
 from .reading import deserialize_energy_system
 
 EnergySystem.from_datapackage = classmethod(deserialize_energy_system)
