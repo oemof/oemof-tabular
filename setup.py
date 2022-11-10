500b19980d4ca87fe4caa298a3c79bd4ae74a1a9
jnnr <32454596+jnnr@users.noreply.github.com>
2022-11-10 17:55:44 +0100

    Request oemof.solph dev in setup.py

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     install_requires=[
         "datapackage==1.5.1",
         "tableschema==1.3.0",
-        "oemof.solph @ git+https://git@github.com/oemof/oemof-solph@v0.4#egg=oemof.solph",
+        "oemof.solph @ git+https://git@github.com/oemof/oemof-solph@dev#egg=oemof.solph",
         "pandas>=0.22",
         "paramiko",
         "toml",
