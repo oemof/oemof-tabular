44184087bbc293a1db9f511d3a07ae274500d6e4
jnnr <32454596+jnnr@users.noreply.github.com>
2022-12-05 14:20:16 +0100

    Add datapackage example for emission constraint

---
+++ src/oemof/tabular/examples/datapackages/emission_constraint/scripts/infer.py
@@ -0,0 +1,14 @@
+"""
+Run this script from the root directory of the datapackage to update
+or create meta data.
+"""
+from oemof.tabular.datapackage import building
+
+
+building.infer_metadata(
+    package_name="oemof-tabular-dispatch-example",
+    foreign_keys={
+        "bus": ["volatile", "dispatchable", "storage", "load", "excess"],
+        "profile": ["load", "volatile"],
+    },
+)
