cc5acdd98714f3fef6af4cd69670583436cd3bec
jnnr <32454596+jnnr@users.noreply.github.com>
2022-12-20 18:27:15 +0100

    Support same python version as solph and tools

--- setup.py
+++ setup.py
@@ -51,8 +51,9 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
