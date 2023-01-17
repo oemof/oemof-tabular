029fb4db4c6c874c00d48d2e6ae52371b1739ec5
jnnr <32454596+jnnr@users.noreply.github.com>
2023-01-17 16:11:14 +0100

    Run all example in compute script

--- src/oemof/tabular/examples/scripting/compute.py
+++ src/oemof/tabular/examples/scripting/compute.py
@@ -12,43 +12,45 @@ from oemof.tabular import datapackage  # noqa
 from oemof.tabular.constraint_facades import CONSTRAINT_TYPE_MAP
 from oemof.tabular.facades import TYPEMAP
 
-name = "dispatch"
+examples = ["dispatch", "investment", "foreignkeys", "emission_constraint"]
+for example in examples:
+    print("Running compute example with datapackage {}".format(example))
 
-# path to directory with datapackage to load
-datapackage_dir = pkg.resource_filename(
-    "oemof.tabular", "examples/datapackages/{}".format(name)
-)
+    # path to directory with datapackage to load
+    datapackage_dir = pkg.resource_filename(
+        "oemof.tabular", "examples/datapackages/{}".format(example)
+    )
 
-# create  path for results (we use the datapackage_dir to store results)
-results_path = os.path.join(
-    os.path.expanduser("~"), "oemof-results", name, "output"
-)
-if not os.path.exists(results_path):
-    os.makedirs(results_path)
+    # create  path for results (we use the datapackage_dir to store results)
+    results_path = os.path.join(
+        os.path.expanduser("~"), "oemof-results", example, "output"
+    )
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
 
-# create energy system object
-es = EnergySystem.from_datapackage(
-    os.path.join(datapackage_dir, "datapackage.json"),
-    attributemap={},
-    typemap=TYPEMAP,
-)
+    # create energy system object
+    es = EnergySystem.from_datapackage(
+        os.path.join(datapackage_dir, "datapackage.json"),
+        attributemap={},
+        typemap=TYPEMAP,
+    )
 
-# create model from energy system (this is just oemof.solph)
-m = Model(es)
+    # create model from energy system (this is just oemof.solph)
+    m = Model(es)
 
-# add constraints from datapackage to the model
-m.add_constraints_from_datapackage(
-    os.path.join(datapackage_dir, "datapackage.json"),
-    constraint_type_map=CONSTRAINT_TYPE_MAP,
-)
+    # add constraints from datapackage to the model
+    m.add_constraints_from_datapackage(
+        os.path.join(datapackage_dir, "datapackage.json"),
+        constraint_type_map=CONSTRAINT_TYPE_MAP,
+    )
 
-# if you want dual variables / shadow prices uncomment line below
-# m.receive_duals()
+    # if you want dual variables / shadow prices uncomment line below
+    # m.receive_duals()
 
-# select solver 'gurobi', 'cplex', 'glpk' etc
-m.solve("cbc")
+    # select solver 'gurobi', 'cplex', 'glpk' etc
+    m.solve("cbc")
 
-results = m.results()
-# now we use the write results method to write the results in oemof-tabular
-# format
-pp.write_results(m, results, results_path)
+    results = m.results()
+    # now we use the write results method to write the results in oemof-tabular
+    # format
+    pp.write_results(m, results, results_path)
