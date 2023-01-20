bd62b5ecb914844097d027f59ce1e7b39f40f4da
jnnr <32454596+jnnr@users.noreply.github.com>
2023-01-20 18:43:29 +0100

    Add image comparison to plot tests

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,12 +1,24 @@
 from pathlib import Path
 
+from matplotlib.testing.compare import compare_images
 import matplotlib.pyplot as plt
 import pandas as pd
+
+from oemof.solph.helpers import extend_basic_path
 
 from oemof.tabular.tools import plots as otaplots
 
 HERE = Path(__file__).parent
 PATH_PLOT_DATA = HERE / "_files" / "plot_data"
+PATH_PLOTS = HERE / "_files" / "plots"
+TEMP_PATH_PLOTS = Path(extend_basic_path("tmp_plots"))
+
+
+def compare_plots(filename, tol=0):
+    expected_plot = PATH_PLOTS / filename
+    temp_plot = TEMP_PATH_PLOTS / filename
+    plt.savefig(temp_plot)
+    assert not compare_images(expected_plot, temp_plot, tol=tol, in_decorator=False), "Plots do not match!"
 
 
 def test_plot_dispatch():
@@ -20,10 +32,12 @@ def test_plot_dispatch():
     otaplots.plot_dispatch(
         ax=ax, df=df, df_demand=df_demand, unit="W"
     )
+    compare_plots("plot_dispatch.png")
 
 
 def test_hourly_plot():
     pass
 
 def test_plot_grouped_bar():
-    otaplots.plot_grouped_bar()
+    pass
+    # otaplots.plot_grouped_bar()
