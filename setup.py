e2c3f866dbead7d63f26962d17f559c5a2232986
Julian.Endres <julian.endres@rl-institut.de>
2023-08-24 17:24:28 +0200

    Use solph release canditate

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     install_requires=[
         "datapackage==1.5.1",
         "tableschema==1.7.4",  # newer versions (v1.8.0 and up) fail!
-        "oemof.solph==0.5.1",
+        "oemof.solph==0.5.1rc1",
         "pandas>=0.22",
         "paramiko",
         "toml",
