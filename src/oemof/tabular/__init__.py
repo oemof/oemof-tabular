86fbc393b3ee87b5d6084696dc2a0394abcbce79
Francesco Witte <github@witte.sh>
2023-08-25 14:36:42 +0200

    Issue depriciation warning for Pythony<3.9

--- src/oemof/tabular/__init__.py
+++ src/oemof/tabular/__init__.py
@@ -1,2 +1,13 @@
 __version__ = "0.0.4dev"
 __project__ = "oemof.tabular"
+
+
+import sys
+import warnings
+
+major, minor, micro = sys.version_info[:3]
+if minor < 9:
+    warnings.warn(
+        f"You are using python version {major}.{minor}.{micro}. oemof.tabular "
+        "will stop supporting python < 3.9 in the future."
+    )
