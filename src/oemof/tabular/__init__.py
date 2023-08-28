7fe40ff3e07c6c96437705964119dc72300e113b
Julian Endres <51374526+nailend@users.noreply.github.com>
2023-08-28 14:45:46 +0200

    Linter

--- src/oemof/tabular/__init__.py
+++ src/oemof/tabular/__init__.py
@@ -7,7 +7,7 @@ import warnings
 
 if sys.version_info[:2] == (3, 8):
     msg = (
-        f"You are using python version 3.8./n"
+        "You are using python version 3.8./n"
         "oemof.tabular will stop supporting python 3.8 in the future."
     )
     warnings.warn(msg, DeprecationWarning, stacklevel=2)
