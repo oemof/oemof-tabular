a5c983a9c332ed6ad660bde290d790807886ab79
Francesco Witte <francesco.witte@dlr.de>
2023-08-28 08:02:32 +0200

    Apply suggestions by @nailend

--- src/oemof/tabular/__init__.py
+++ src/oemof/tabular/__init__.py
@@ -5,9 +5,9 @@ __project__ = "oemof.tabular"
 import sys
 import warnings
 
-major, minor, micro = sys.version_info[:3]
-if minor < 9:
-    warnings.warn(
-        f"You are using python version {major}.{minor}.{micro}. oemof.tabular "
-        "will stop supporting python < 3.9 in the future."
+if sys.version_info[:2] == (3, 8):
+    msg = (
+        f"You are using python version 3.8./n"
+        "oemof.tabular will stop supporting python 3.8 in the future."
     )
+    warnings.warn(msg, DeprecationWarning, stacklevel=2)
