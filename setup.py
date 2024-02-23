20376cfb67e4d7ee5d0200046c65065c63e0bd5b
Julian.Endres <julian.endres@rl-institut.de>
2024-02-23 18:15:48 +0100

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*names, **kwargs):
 
 setup(
     name="oemof.tabular",
-    version="0.0.5dev",
+    version="0.0.5",
     license="BSD 3-Clause License",
     description="Load oemof energy systems from tabular data sources.",
     long_description="%s\n%s"
