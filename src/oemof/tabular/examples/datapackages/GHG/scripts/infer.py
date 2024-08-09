8780416280d6e1a7a14c2b9a18b26f2fecf2ce78
SabineHaas <sabine.haas@rl-institut.de>
2024-08-09 15:32:13 +0200

    Add example data for CommodityGHG and ConversionGHG facades

---
+++ src/oemof/tabular/examples/datapackages/GHG/scripts/infer.py
@@ -0,0 +1,25 @@
+"""
+Run this script from the root directory of the datapackage to update
+or create meta data.
+"""
+
+from oemof.tabular.datapackage import building
+
+# This part is for testing only: It allows to pass
+# the filename of inferred metadata other than the default.
+if "kwargs" not in locals():
+    kwargs = {}
+
+building.infer_metadata(
+    package_name="GHG-test",
+    foreign_keys={
+        "bus": ["commodity_ghg", "load", "excess", "gas_import"],
+        "profile": ["load"],
+        "from_bus": ["conversion_ghg"],
+        "to_bus": ["conversion_ghg"],
+        "emission_bus_0": ["conversion_ghg", "commodity_ghg"],
+        "emission_bus_1": ["conversion_ghg"],
+        "emission_bus_2": ["conversion_ghg"],
+    },
+    **kwargs,
+)
