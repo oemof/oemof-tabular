2f6eccccd7fa489aaddbbd25095c7db0ba86d504
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2026-01-16 10:29:28 +0100

    Fix bus balances in datapackages

--- src/oemof/tabular/facades/bus.py
+++ src/oemof/tabular/facades/bus.py
@@ -1,9 +1,9 @@
-from oemof.network.network import Node
+from oemof.solph.buses import Bus as Bus_
 
 from oemof.tabular._facade import Facade
 
 
-class Bus(Facade, Node):
+class Bus(Facade, Bus_):
     def __init__(self, label: str, **kwargs):
         super().__init__(label=label, **kwargs)
 
